bf480f89efb30d878fcdc04c9f17c3400d8c7069
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-01 17:12:46 -0300

    added protection

--- src/routes/questionRouter/questionRouter.js
+++ src/routes/questionRouter/questionRouter.js
@@ -4,9 +4,9 @@ const {ValidateQuestion} = require("../../models/questions")
 const authToken = require("../../middleware/authToken")
 
 questionRouter.post("/", [authToken, ValidateQuestion], handleQuestions.askQuestion)
-questionRouter.get("/",  handleQuestions.getQuestions)
-questionRouter.get("/:questionId", handleQuestions.getOneQuestion)
-questionRouter.patch("/:questionId", handleQuestions.setAsResolved)
+questionRouter.get("/",   authToken, handleQuestions.getQuestions)
+questionRouter.get("/:questionId",  authToken, handleQuestions.getOneQuestion)
+questionRouter.patch("/:questionId",  authToken, handleQuestions.setAsResolved)
 questionRouter.get("/by/:username", authToken, handleQuestions.getQuestionsByUser)
 
 module.exports = questionRouter;
--- src/routes/userRouter/userRouter.js
+++ src/routes/userRouter/userRouter.js
@@ -1,15 +1,17 @@
 const usersRouter = require('express').Router()
 const handleUsers = require("../../controllers/usersControllers")
 const {ValidateUser} = require("../../models/user")
+const authToken = require("../../middleware/authToken")
+
 
 usersRouter.post("/signin", ValidateUser, handleUsers.addUser)
 
 usersRouter.post("/login", handleUsers.logInUser)
 
-usersRouter.get('/', handleUsers.getUsers)
+usersRouter.get('/',  authToken, handleUsers.getUsers)
 
-usersRouter.get('/:username', handleUsers.getUserData)
+usersRouter.get('/:username',  authToken, handleUsers.getUserData)
 
-usersRouter.patch('/profile-pic', handleUsers.updateProfilePicture)
+usersRouter.patch('/profile-pic', authToken, handleUsers.updateProfilePicture)
 
 module.exports = usersRouter;
