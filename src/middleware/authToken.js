9614e46b7c211bb0272c97b44d22c5b8b424c068
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-01 16:11:29 -0300

    keeping secret in a variable

--- src/middleware/authToken.js
+++ src/middleware/authToken.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken')
 
+const SECRET = ASDLAHSDJHVAGDAISD789;
 
 function authenticateToken(req, res, next){
     const token = req.headers['x-access'];
     if(!token){
         res.status(400).json({message : 'no token'})
     }else{
-        jwt.verify(token, 'ASDLAHSDJHVAGDAISD789', (err)=>{
+        jwt.verify(token, SECRET, (err)=>{
             if(err){
                 res.status(403).json({message: "invalid token"})
             }else{
