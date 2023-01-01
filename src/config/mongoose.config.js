106f2387e16becc3bb4158b8dca2718a4105c7b6
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-01 16:49:23 -0300

    asd

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-const mongoConnection = mongoose.connect(env.MONGO_URI)
+const mongoConnection = mongoose.connect(process.env.MONGO_URI)
     .then(()=>{console.log('Connection established')})
     .catch(error => console.log(error));
     mongoose.set('strictQuery', false)
