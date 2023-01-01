d261116c1399fb4350b9f62a749f61da8ca93aea
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-01 17:22:25 -0300

    ok

--- src/routes/questionRouter/questionRouter.js
+++ src/routes/questionRouter/questionRouter.js
@@ -4,7 +4,7 @@ const {ValidateQuestion} = require("../../models/questions")
 const authToken = require("../../middleware/authToken")
 
 questionRouter.post("/", [authToken, ValidateQuestion], handleQuestions.askQuestion)
-questionRouter.get("/",   authToken, handleQuestions.getQuestions)
+questionRouter.get("/",  handleQuestions.getQuestions)
 questionRouter.get("/:questionId",  authToken, handleQuestions.getOneQuestion)
 questionRouter.patch("/:questionId",  authToken, handleQuestions.setAsResolved)
 questionRouter.get("/by/:username", authToken, handleQuestions.getQuestionsByUser)
