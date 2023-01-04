e08429d3350b7dac7a1ad10f06d4fa571aa194a9
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-04 12:53:43 -0300

    back to local

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer,  {
     cors: {
-        origin: "https://devplacetalk.firebaseapp.com"
+        origin: "http://localhost:3000/"
     }});
 
 
