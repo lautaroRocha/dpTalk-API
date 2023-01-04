b63764ca666648958181a47f270b170c6869985f
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-04 12:51:22 -0300

    adsa

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer,  {
     cors: {
-        origin: "https://devplacetalk.firebaseapp.com/"
+        origin: "https://devplacetalk.firebaseapp.com"
     }});
 
 
