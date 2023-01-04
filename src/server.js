45ad03fe45ffcda9a0048e7ff84e104a0fdc200a
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-04 14:20:47 -0300

    moved socket out of the api

--- src/server.js
+++ src/server.js
@@ -4,15 +4,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT
 const cors = require('cors')
-// const { createServer } = require("http");
-// const { Server } = require("socket.io");
 const router = require("./routes/router")
-// const httpServer = createServer(app);
-
-// const io = new Server(httpServer,  {
-//     cors: {
-//         origin: "http://localhost:3000/"
-//     }});
 
 
 app.use(express.json())  
@@ -22,24 +14,6 @@ app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
     }
  );
-
-// io.on('connection', (socket) => {
-//     console.log(`⚡: ${socket.id} user just connected!`);
-//     socket.on('disconnect', () => {
-//       console.log('🔥: A user disconnected');
-//     });
-//     socket.on('new-confirmed', (socket) => {
-//         io.emit('confirmed-notification', JSON.stringify(socket))
-//     });
-//     socket.on('new-answer', (socket) => {
-//         io.emit('answer-notification', JSON.stringify(socket))
-//     });
-//     socket.on('new-liked', (socket) => {
-//         io.emit('like-notification', JSON.stringify(socket))
-//     });
-// });
-
-
 
 app.use(router)
 
