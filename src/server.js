554945994e030ec593ca9ee312f53aff2859c1a8
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-04 12:48:42 -0300

    accept proxy from dvplacetalk

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer,  {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "https://devplacetalk.firebaseapp.com/"
     }});
 
 
