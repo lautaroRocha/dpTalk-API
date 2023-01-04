cb8c7b94700e642a836c5e5f3e8b340976d40bd6
Lautaro Rocha <lautarovrocha@gmail.com>
2023-01-04 12:09:56 -0300

    managing socket events for notifications

--- src/server.js
+++ src/server.js
@@ -4,16 +4,47 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT
 const cors = require('cors')
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const router = require("./routes/router")
+const httpServer = createServer(app);
+
+const io = new Server(httpServer,  {
+    cors: {
+        origin: "http://localhost:3000"
+    }});
+
 
 app.use(express.json())  
-app.use(express.urlencoded( {extended : true } ))
+app.use(express.urlencoded( {extended : false } ))
 app.use(cors())
-
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
     }
  );
 
+io.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+    socket.on('new-confirmed', (socket) => {
+        io.emit('confirmed-notification', JSON.stringify(socket))
+    });
+    socket.on('new-answer', (socket) => {
+        io.emit('answer-notification', JSON.stringify(socket))
+    });
+    socket.on('new-liked', (socket) => {
+        io.emit('like-notification', JSON.stringify(socket))
+    });
+});
+
+
+
+
+httpServer.listen(8000);
+
 app.use(router)
+
+module.exports = io 
 
